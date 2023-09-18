23f86b805d1b2f03f7b552dab998d47aa6e26c01
Colson <colsoncalifornication@gmail.com>
2023-09-18 15:01:46 +0545

    connect redux to react

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import RepositoriesList from "./RepositoriesList";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Search for a NPM Package</h1>
+      <RepositoriesList />
+    </div>
+  );
+};
+
+export default App;
